ad4c78afa4693a70be1eca64909a30fbfd62a243
beom-jun <chlrhwns1@naver.com>
2024-10-30 17:10:42 +0900

    재시작

--- src/pages/StartApp.jsx
+++ src/pages/StartApp.jsx
@@ -7,7 +7,7 @@ const Start = () => {
     <div className='standard'>
       <div className='start-text'>
         닉네임을 입력하여 <br />
-        MUTE와 함께 하루를 기록해요!
+        MUTE와 함께 하루를 기록해요!.
       </div>
 
       <div className='img-part'>
