cf74b48a9783e2ad6364375afde67cea8482a052
thisjheok <jhjang3344@gmail.com>
2024-10-31 19:01:05 +0900

    FIX_Writing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
-// import Writing from './pages/Writing/Writing'
-// import Calendar from './pages/Calender/Calender'
+import Writing from './pages/Writing/Writing'
+import Calendar from './pages/Calender/Calender'
+// import Start from './pages/StartApp'
+import EmotionMusicPage from './pages/Emotional/EmotionMusicPage'
 import Start from './pages/StartApp'
-// import EmotionMusicPage from './pages/Emotional/EmotionMusicPage'
-
 function App() {
   return (
     <>
       {/* <EmotionMusicPage/> */}
-      <Start/>
-      {/* <Calendar/> */}
+      {/* <Start/> */}
+      <Calendar/>
       {/* <Writing/> */}
     </>
   )
--- src/pages/StartApp.jsx
+++ src/pages/StartApp.jsx
@@ -4,19 +4,24 @@ import logo from '../../src/assets/logo.png'
 
 const Start = () => {
   return (
-    <div className='standard'>
-      <div className='start-text'>
-        닉네임을 입력하여 <br />
-        <span>Mute</span>와 함께 하루를 기록해요!
-        <div className='start-marking'></div>
-      </div>
+    <div className='start-container'>
+      <div className='start-content'>
+        <div className='start-text'>
+          닉네임을 입력하여 <br />
+          <span>Mute</span>와 함께 하루를 기록해요!
+          <div className='start-marking'></div>
+        </div>
 
-      <div className='img-part'>
-        <img src={logo} alt="" style={{}}/>
-      </div>
+        <div className='img-part'>
+          <img src={logo} alt="로고" />
+        </div>
 
-      <input
-        type='text' className='nickname-part' placeholder='닉네임 입력'/>
+        <input
+          type='text' 
+          className='nickname-part' 
+          placeholder='닉네임 입력'
+        />
+      </div>
     </div>
   );
 };
