e5182b2b24f5faa1f532a119b727f9100a970732
beom-jun <chlrhwns1@naver.com>
2024-10-31 15:02:34 +0900

    시작페이지, 캘린더 부분 ui 완성

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 // import Writing from './pages/Writing/Writing'
-import Calendar from './pages/Calender/Calender'
-// import Start from './pages/StartApp'
+// import Calendar from './pages/Calender/Calender'
+import Start from './pages/StartApp'
 // import EmotionMusicPage from './pages/Emotional/EmotionMusicPage'
 
 function App() {
   return (
     <>
       {/* <EmotionMusicPage/> */}
-      {/* <Start/> */}
-      <Calendar/>
+      <Start/>
+      {/* <Calendar/> */}
       {/* <Writing/> */}
     </>
   )
